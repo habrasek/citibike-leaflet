66a0ec812397cb98a1a028eb2c85c26c76796898
habrasek <65578198+habrasek@users.noreply.github.com>
2021-07-17 11:41:55 -0400

    yes

--- Unsolved/files/static/js/logic.js
+++ Unsolved/files/static/js/logic.js
@@ -1,15 +1,66 @@
-// var newYorkCoords = [40.73, -74.0059];
+// var newYorkCoords = [0.73, -74.0059];4
+
 // var mapZoomLevel = 12;
 
+let url = 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json';
 // Create the createMap function.
+let response = d3.json(url);
+
+console.log(response);
+
+function createMap(bikeLayer){
+
+  let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+  let baseMaps = {
+    "Street Map":streetmap
+  }
+
+  let overlayMaps = {
+    "Bike Stations":bikeLayer
+  }
+
+  let map = L.map("map-id", {
+    center : [40.73, -74],
+    zoom:12,
+    layers: [streetmap, bikeLayer]
+  })
+
+  L.control(baseMaps, overlayMaps, {
+    collapsed:false
+  }).addTo(map)
+
+};
+
+
+
 
 
   // Create the tile layer that will be the background of our map.
+function createMarkers(response){
+  let stations = response.data.stations;
+  console.log(stations);
 
+  
+  let bikeMarkers = [];
+
+  stations.forEach(station =>{
+    let bikeMarker = L.marker([station.lat, station.lon])
+    .binfPopup(`Station Name: ${station.name}`)
+
+    bikeMarkers.push(bikeMarker);
+  })
+
+  let bikeLayer = L.layerGroup(bikeMarkers);
+
+  createMap(bikeLayer);
+};
 
   // Create a baseMaps object to hold the lightmap layer.
 
-
+createMap();
   // Create an overlayMaps object to hold the bikeStations layer.
 
 
